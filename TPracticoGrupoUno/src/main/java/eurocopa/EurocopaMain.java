1f52434e8fad7fee65bd41f13de4c0378cc69661
nancy091965 <nancyquerol@hotmail.com>
2023-04-15 21:35:16 -0300

    el main ya lee el resultado de partidos, los csv los puse en test/resources
    hice el main que lee el resultados de partidos y puse los archivos  csv en el test/resources

--- TPracticoGrupoUno/src/main/java/eurocopa/EurocopaMain.java
+++ TPracticoGrupoUno/src/main/java/eurocopa/EurocopaMain.java
@@ -1,7 +1,30 @@
 package eurocopa;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.util.List;
 
 public class EurocopaMain {
-    public static void main(String[] args) {
+    
 
+	public static void main(String[] args) {
+//LEEMOS LOS RESULTADOS
+    	
+    	Path pathResultados = Paths.get("src/test/resources/resultados de partidos jugados.csv");
+    List<String> lineasResultados=null;
+	try {
+		lineasResultados = Files.readAllLines (pathResultados);
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		System.out.println ("no se puede leer");
+		System.exit(1);
+	}
+   
+    for(String lineaResultado:lineasResultados){
+    
+	System.out.println (lineaResultado);
+       
     }
+	}
 }
